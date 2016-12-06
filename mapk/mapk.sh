09413337d2faccd1c72e3cbbfcd6e1aa014d2b8d
lhui2010 <lhui2010@gmail.com>
2016-12-06 11:15:12 +0800

    add mapk discovery pipeline

---
+++ mapk/mapk.sh
@@ -0,0 +1,5 @@
+find_motif.pl TxYVxxRWYRAPE pasa2.longest.filter.pep >MAPK.pep
+find_motif.pl [ST]xxGTxxYMxPER pasa2.longest.filter.pep >MAPKK.pep
+find_motif.pl DIWSxGCTxxExxTxxxP pasa2.longest.filter.pep >MAPKKK_MEKK.pep
+find_motif.pl GVxxWELxTxxxPW  pasa2.longest.filter.pep >MAPKKK_Raf.pep
+
--- mcscan_chr_count_stat/join.pl
+++ mcscan_chr_count_stat/join.pl
@@ -15,13 +15,20 @@ $_=<>;
 chomp;
 my ($lid,$lqry_tig, $lqry_start, $lqry_end, $ltig, $lstart, $lend, $lstrand)  = split; #sorted
 my $lprint = $_;
+
+my %classify_chr;
+my %chr;
 while(<>)
 {
     chomp;
     my ($id,$qry_tig, $qry_start, $qry_end, $tig, $start, $end, $strand) = split;
+    
+#to print header
+    $chr{$tig} = 1;
 
     if ($qry_tig ne $lqry_tig or $tig ne $ltig or $start > $lend)
     {
+        $classify_chr{$lqry_tig}{$ltig}+=$lend-$lstart;
         print $lprint,"\n";
         ($lid,$lqry_tig, $lqry_start, $lqry_end, $ltig, $lstart, $lend, $lstrand)=
         ($id,$qry_tig, $qry_start, $qry_end, $tig, $start, $end, $strand);
@@ -35,3 +42,36 @@ while(<>)
 }
 
 print $_,"\n";
+
+open OUT, ">contig_chr_stat_table" or die;
+print OUT "Contig";
+print OUT "\t$_" for (sort keys %chr);
+#also output maximum aligned chr, or putative location chr of contig
+print OUT "\tmax";
+print OUT "\n";
+for my $k (sort keys %classify_chr)
+{
+    print OUT $k;
+    #for my $chr (sort {$classify_chr{$k}{$b} <=> $classify_chr{$k}{$a}} keys %{$classify_chr{$k}})
+    my $maxid = "chr0";
+    my $maxaln= 0;
+    for my $chr (sort keys %chr)
+    {
+        if (exists $classify_chr{$k}{$chr})
+        {
+            print OUT "\t$classify_chr{$k}{$chr}";
+            
+            if($classify_chr{$k}{$chr} > $maxaln and $chr ne "chr0") #a hack to sovle exclussive chr0 problem
+            {
+                $maxaln = $classify_chr{$k}{$chr};
+                $maxid = $chr;
+            }
+        }
+        else
+        {
+            print OUT "\t0";
+        }
+    }
+    print OUT "\t$maxid";
+    print OUT "\n";
+}
