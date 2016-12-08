f4db537aae7de88546fecfa6e40402c383434818
lhui2010 <lhui2010@gmail.com>
2016-12-08 16:24:37 +0800

    add extract_func_from_KEGG_NR.pl to convert KEGG.result and NR.result into table

---
+++ extract_func_from_KEGG_NR.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+use strict;
+
+#InterproScan
+#evm.model.tig00000281_pilon.61  20D84E4822E79A9A        464     HMMSmart        SM00369 Leucine-rich repeats, typical (most populate
+#Blast against NR
+#QueryId        QueryLen        QueryStart      QueryEnd        SubjectId       SubjectLen      SubjectStart    SubjectEnd      Identities%     Positives%      QueryCovery%    BitScore        Evalue  SubjectDescription
+
+# a hash storing everything
+my %func;
+
+open IPR, "IPRSCAN.result" or die;
+while(<IPR>)
+{
+    chomp;
+    my ($pep_id,$crc64, $pep_len, $method, $db_entry, $func_desc, $start_match, $end_match, $eval, $match_status, $date, $interpro_entry, $intropro_entry_desc, $GO) = split /\t/, $_;
+    $func{$pep_id}.="IPR:$func_desc//";
+}
+
+open NR, "NR.result" or die;
+while(<NR>)
+{
+    chomp;
+    my ($QueryId, $QueryLen, $QueryStart, $QueryEnd, $SubjectId, $SubjectLen, $SubjectStart, $SubjectEnd, $Identities, $Positives, $QueryCovery, $BitScore, $Evalue, $SubjectDescription) = split /\t/, $_;
+    $func{$QueryId} .= "$SubjectDescription//";
+}
+
+for my $k(sort keys %func)
+{
+    print $k, "\t", $func{$k},"\n";
+}
+
