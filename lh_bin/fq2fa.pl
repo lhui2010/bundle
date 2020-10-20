0369a69a4f2770294409731b6fd84d25da4ef26e
lhui2010 <lhui2010@gmail.com>
2020-10-20 12:27:30 +0800

    update

--- lh_bin/fq2fa.pl
+++ lh_bin/fq2fa.pl
@@ -1,28 +1,9 @@
 #!/usr/bin/perl -w
 
-$fname=$ARGV[0];
-
-@e=split/\./, $fname;
-$oname=$e[0].".fq";
-
-open FIN, $fname;
-open FOUT, ">$oname"||die "can't create output file\n";
-
-while(<FIN>)
+while(<>)
 {
-chomp;
-$iden=substr $_, 0, 1;
-if($iden && $iden eq ">")
-{
-$line=substr($_, 1);
-chomp($seq=<FIN>);
-
-print FOUT "\@$line\n$seq\n+\n";
-
-for($i=0; $i<length($seq); $i++)
-{
-print FOUT "I";
-}
-print FOUT "\n";
-}
+    $_=<>;
+    print;
+    $_=<>;
+    $_=<>;
 }
