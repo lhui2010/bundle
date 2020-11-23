d51253a66af40202fbaa437370259a4626641172
lhui2010@gmail.com <lhui2010@gmail.com>
2020-11-23 18:21:51 +0800

    IDE change

--- wrapper/iga/apps/base.py
+++ wrapper/iga/apps/base.py
@@ -56,11 +56,10 @@ def mkdir(dirname, overwrite=False):
 
 def sh(cmd):
     logger.info(cmd)
-    prior_cmd = 'set -euo pipefail\n'
+    prior_cmd = 'set -eo pipefail\n'
     subprocess.run(prior_cmd + cmd, shell=True)
 
-
-conda_act = r"""
+"""
 export PS1="(base) \[\033]2;\h:\u $PWD\007\033[33;1m\]\u@\h \033[35;1m\t\n\033[0m\[\033[36;1m\]$PWD\[\033[0m\]\n\[\e[32;1m\]$\[\033[0m\]"
 export CONDA_BACKUP_HOST=x86_64-conda_cos6-linux-gnu
 export CONDA_EXE=/ds3200_1/users_root/yitingshuang/lh/anaconda3/bin/conda
@@ -72,6 +71,9 @@ export CONDA_DEFAULT_ENV=base
 export CONDA_PREFIX=/ds3200_1/users_root/yitingshuang/lh/anaconda3
 export CONDA_SHLVL=1
 export _CONDA_PYTHON_SYSCONFIGDATA_NAME_USED="_sysconfigdata_x86_64_conda_cos6_linux_gnu"
+"""
+
+conda_act = r"""
 source ~/lh/anaconda3/etc/profile.d/conda.sh
 conda activate {}
 """
