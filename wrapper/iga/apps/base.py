bdfd3267adba35aa84e0b19b375f01f820f81feb
lhui2010@gmail.com <lhui2010@gmail.com>
2020-11-27 13:05:45 +0800

    IDE change

--- wrapper/iga/apps/base.py
+++ wrapper/iga/apps/base.py
@@ -88,10 +88,10 @@ def bsub(cmd, queue='Q104C512G_X4'):
     :return:
     """
     ret = ''
-    logger.info(cmd)
     bsub_cmd = 'bsub -q {}  -o output.%J -e error.%J '.format(queue)
     prior_cmd = 'set -eo pipefail\n'
     cmd_full = bsub_cmd + '"' + prior_cmd + cmd + '"'
+    logger.info(cmd_full)
     #ret = subprocess.check_output(bsub_cmd + '"' + prior_cmd + cmd + '"', shell=True).decode()
     ret = subprocess.check_output(cmd_full, shell=True).decode()
     job_id = parse('Job < {} > is submitted to queue < ' + queue + ' >.', ret.rstrip())
