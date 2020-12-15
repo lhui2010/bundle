17f907f3df6ef95655fcaedf9ce166591ac7623d
lhui2010@gmail.com <lhui2010@gmail.com>
2020-12-15 17:28:16 +0800

    IDE change

--- wrapper/iga/annotation/function.py
+++ wrapper/iga/annotation/function.py
@@ -4,7 +4,8 @@ functional annotation: iprscan
 
 #0 input pep
 #1 threads
-from iga.apps.base import bsub, emain
+from iga.apps.base import bsub, emain, waitjob
+import os.path as op
 
 iprscan_sh = """
 interproscan.sh -i {0} -b cornev1.0.pep.out -cpu {1} -f tsv -iprlookup --goterm -pa -dp"""
@@ -15,8 +16,9 @@ def iprscan(pep_file=None, threads=30):
     :param pep_file:
     :return:
     """
+    job_name = "ipr." + op.basename(pep_file)
     cmd = iprscan_sh.format(pep_file, threads)
-    job = bsub(cmd)
+    job = bsub(cmd, cpus=threads, name=job_name)
     waitjob(job)
 
 if __name__ == "__main__":
