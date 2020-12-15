03fb61bfa505d8289097db40abd8f8e47b96a665
lhui2010@gmail.com <lhui2010@gmail.com>
2020-12-15 12:46:28 +0800

    IDE change

--- wrapper/iga/apps/blast.py
+++ wrapper/iga/apps/blast.py
@@ -1,6 +1,8 @@
 """
 blast related wrappers
 """
+from collections import defaultdict
+
 from iga.apps.base import emain, bsub, wait_until_finish
 
 #0 ref
@@ -8,19 +10,47 @@ from iga.apps.base import emain, bsub, wait_until_finish
 #2 output
 blastp_sh=r"""
 makeblastdb -in {0} -dbtype prot
-blastp -db {0} -query {1} -out {2}.bln -evalue 1e-5 -outfmt 7 -num_threads 5
+blastp  -num_threads 5 -db {0} -query {1} -out {2}.bln -evalue 1e-5 -outfmt 7
 """
 
-def blastp(ref=None, qry=None):
+def blastp(ref=None, qry=None, threads=5):
     r"""
     :param query:
     :param ref:
     :return:
     """
     cmd = blastp_sh.format(ref, qry, qry)
-    job = bsub(cmd, cpus=5)
+    job = bsub(cmd, cpus=threads)
     wait_until_finish(job)
     return 0
+
+
+def filter_reciprocal_best(bln=None):
+    qry_best = {}
+    qry_line = {}
+    ref_best = {}
+    highest = defaultdict(int)
+    with open(bln) as fh:
+        for line in fh:
+            if line.startswith("#"):
+                continue
+            mylist = line.rstrip().split()
+            qry = mylist[0]
+            ref = mylist[1]
+            bitscore = int(mylist[-1])
+            if(bitscore > highest[qry]):
+                highest[qry] = bitscore
+                qry_best[qry] = ref
+                qry_line = line.rstrip()
+            if(bitscore > highest[ref]):
+                highest[ref] = bitscore
+                ref_best[ref] = qry
+    for k in qry_best:
+        this_best_ref = qry_best[k]
+        if(ref_best[this_best_ref] == k):
+            #Reciprocal best
+            print(qry_line[k])
+
 
 if __name__ == "__main__":
     emain()
