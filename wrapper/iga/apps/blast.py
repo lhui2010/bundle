a780b8f0d545471c084cba21b9f3f04a199440a0
lhui2010@gmail.com <lhui2010@gmail.com>
2020-12-15 09:30:44 +0800

    IDE change

---
+++ wrapper/iga/apps/blast.py
@@ -0,0 +1,26 @@
+"""
+blast related wrappers
+"""
+from iga.apps.base import emain, bsub, wait_until_finish
+
+#0 ref
+#1 qry
+#2 output
+blastp_sh=r"""
+makeblastdb -in {0} -dbtype prot
+blastp -db {0} -query {1} -out {2}.bln -evalue 1e-5 -outfmt 7 -num_threads 5
+"""
+
+def blastp(query=None, ref=None):
+    r"""
+    :param query:
+    :param ref:
+    :return:
+    """
+    cmd = blastp_sh.format(ref, query)
+    job = bsub(cmd)
+    wait_until_finish(job)
+    return 0
+
+if __name__ == "__main__":
+    emain()
