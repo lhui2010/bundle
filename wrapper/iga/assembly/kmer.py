4bd78abe0c546e50903bdfc7816d415bbd558a3c
lhui2010@gmail.com <lhui2010@gmail.com>
2020-12-15 16:40:29 +0800

    IDE change

--- wrapper/iga/assembly/kmer.py
+++ wrapper/iga/assembly/kmer.py
@@ -18,7 +18,7 @@ import logging
 # 2 threads
 # 3 kmer_size
 # 4 output
-from apps.base import emain, sh, wait_until_finish, bsub
+from iga.apps.base import emain, sh, wait_until_finish, bsub
 
 # 0 fastq.gz
 # 1 prefix,
@@ -29,6 +29,7 @@ genomescope_sh = """
 zcat {0} >{1}.cat.fq
 /ds3200_1/users_root/yitingshuang/lh/projects/buzzo/kmer/bin/jellyfish-linux count -C -m {3} -s 1000000000 -t {2} {1}.cat.fq -o {1}.jf 
 /ds3200_1/users_root/yitingshuang/lh/projects/buzzo/kmer/bin/jellyfish-linux histo -t {2} {1}.jf > {1}.reads.histo
+rm {1}.cat.fq {1}.jf
 Rscript /ds3200_1/users_root/yitingshuang/lh/projects/buzzo/kmer/bin/genomescope/genomescope.R {1}.reads.histo {3} 150 {4}
 """
 
@@ -76,6 +77,7 @@ echo -e "{2}\t$GenomeSize\t$Heterozygosity\t$Repeat"
 
 """
 
+
 def gce(fastq=None, prefix='', threads=64, kmer=23, workdir=''):
     # assembly, subreads
     if prefix == '':
@@ -88,6 +90,7 @@ def gce(fastq=None, prefix='', threads=64, kmer=23, workdir=''):
     wait_until_finish(job)
     return 0
     # subprocess.run(cmd, shell = T
+
 
 # def main():
 #     prog_name = "kmer_wrapper"
