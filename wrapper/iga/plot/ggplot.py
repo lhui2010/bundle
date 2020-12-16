df7bae34e0615f8b3d3249166dd033f90676ed8a
lhui2010@gmail.com <lhui2010@gmail.com>
2020-12-16 13:02:15 +0800

    IDE change

--- wrapper/iga/plot/ggplot.py
+++ wrapper/iga/plot/ggplot.py
@@ -63,11 +63,11 @@ ggsave("{0}.pdf", width = 5, height = 5)
 
 
 def barplot(table=None, x='', y='', group='', theme='publication'):
-    if(x=''):
+    if x == '':
         with open(table) as fh:
             header = fh.readline()
             (x,y,group) = header.rstrip().split()
-    if(theme != ""):
+    if theme != "":
         theme = "+theme_" + theme + "()"
     cmd = theme_publication_r + barplot_r.format(table, x, y, group, theme)
     rscript(cmd)
