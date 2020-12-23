3a5a294c04ab96e37bce769a3f9e13b44a017c73
lhui2010@gmail.com <lhui2010@gmail.com>
2020-12-23 13:24:23 +0800

    IDE change

--- wrapper/iga/assembly/polish.py
+++ wrapper/iga/assembly/polish.py
@@ -4,12 +4,7 @@ import argparse
 import textwrap
 import subprocess
 
-# class CTL():
-
-# Give arguments, will return the specific CTLs
-# Support Direct Print
-# Support tag value change
-from iga.apps.base import conda_act, get_prefix, bsub
+from iga.apps.base import conda_act, get_prefix, bsub, emain
 
 # 0 contig.fa [abs path]
 # 1 sgs.fq.gzs [abs path]
@@ -89,34 +84,42 @@ rm {0}.bam
 """
 
 
-def gcpp(ctg_file, bam_file, threads=20):
+def gcpp(contig=None, bam=None, threads=100):
+    r"""
+    Polish Pacbio Assembly with Pacbio Reads
+    %s contig bam
+    :param contig: Raw assembly
+    :param bam: subreads bam
+    :param threads: default=100"
+    :return:
+    """
     # assembly, subreads
-
-    cmd = conda_act.format('falcon') + gcpp_sh.format(ctg_file, bam_file, threads)
+    prefix = get_prefix(contig)
+    cmd = conda_act.format('falcon') + gcpp_sh.format(contig, bam, threads)
     #    cmd = conda_act
-    subprocess.run(cmd, shell=True)
+    bsub(cmd, cpus=threads, name="gcpp" + prefix)
 
 
-def main():
-    prog_name = "Polish Pacbio Assembly with Pacbio Reads"
-    usage = "Polish Pacbio Assembly with Pacbio Reads"
-
-    parser = argparse.ArgumentParser(
-        prog=prog_name,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=textwrap.dedent(usage),
-        epilog="")
-    parser.add_argument("CONTIG", help="Raw assembly")
-    parser.add_argument("BAM", help="subreads bam")
-    parser.add_argument("-t", "--threads", default=100, type=int, help="flanking distance default (1000)")
-    args = parser.parse_args()
-
-    ctg_file = args.CONTIG
-    bam_file = args.BAM
-    gcpp(ctg_file, bam_file)
+# def main():
+#     prog_name = "Polish Pacbio Assembly with Pacbio Reads"
+#     usage = "Polish Pacbio Assembly with Pacbio Reads"
+#
+#     parser = argparse.ArgumentParser(
+#         prog=prog_name,
+#         formatter_class=argparse.RawDescriptionHelpFormatter,
+#         description=textwrap.dedent(usage),
+#         epilog="")
+#     parser.add_argument("CONTIG", help="Raw assembly")
+#     parser.add_argument("BAM", help="subreads bam")
+#     parser.add_argument("-t", "--threads", default=100, type=int, help="flanking distance default (1000)")
+#     args = parser.parse_args()
+#
+#     ctg_file = args.CONTIG
+#     bam_file = args.BAM
+#     gcpp(ctg_file, bam_file)
 
 
 #    flanking_distance = args.flanking
 
 if __name__ == "__main__":
-    main()
+    emain()
