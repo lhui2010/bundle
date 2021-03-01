e2ff6bf4edc408868e938cea4399d337ad99a0a4
lhui2010@gmail.com <lhui2010@gmail.com>
2021-03-01 13:38:21 +0800

    genblast debug

--- wrapper/iga/annotation/genblast.py
+++ wrapper/iga/annotation/genblast.py
@@ -134,7 +134,7 @@ def filter_early_stop(fasta=None):
     genome_dict = {}
     # rename genome_dict
     for old_key in raw_dict.keys():
-        if (re.search(r'\*|U[ATCG]', raw_dict[old_key].seq.__str__())):
+        if re.search(r'\*|U[ATCG]', raw_dict[old_key].seq.__str__()):
             logging.debug('{} with seq {} contains N'.format(old_key, raw_dict[old_key].seq))
             continue
         else:
