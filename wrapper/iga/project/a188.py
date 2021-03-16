cc042427d90b989e05d4bbc03dc7a47497b6bb85
lhui2010@gmail.com <lhui2010@gmail.com>
2021-03-16 13:34:30 +0800

    Ortho to bedpe

--- wrapper/iga/project/a188.py
+++ wrapper/iga/project/a188.py
@@ -872,9 +872,10 @@ def annotate_block(anchor_file=None, qbed='', sbed=''):
         else:
             strand = '+'
         block_name = "Block{0}".format(i)
-        print(block_name)
-        header += "\t".join([qry_chr, qry_start, qry_end, sub_chr, sub_start, sub_end,
-                             block_name, strand])
+        tmp = "{}\t" * 8
+        tmp = tmp.rstrip()
+        header += tmp.format(qry_chr, qry_start, qry_end, sub_chr, sub_start, sub_end,
+                             block_name, strand)
         content = syn_content[i]
         print(header + "\n" + content)
 
