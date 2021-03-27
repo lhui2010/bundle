866aca3a6be3e6bc95042e31bc1a7b49863bfb6e
lhui2010@gmail.com <lhui2010@gmail.com>
2021-03-27 17:09:35 +0800

    blastn

--- wrapper/iga/apps/blast.py
+++ wrapper/iga/apps/blast.py
@@ -11,13 +11,14 @@ import coloredlogs
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='DEBUG', logger=logger)
 
-#0 ref
-#1 qry
-#2 output
-blastp_sh=r"""
+# 0 ref
+# 1 qry
+# 2 output
+blastp_sh = r"""
 makeblastdb -in {0} -dbtype prot
 blastp  -num_threads 5 -db {0} -query {1} -out {2}.bln -evalue 1e-5 -outfmt 7
 """
+
 
 def blastp(ref=None, qry=None, threads=5):
     r"""
@@ -28,6 +29,27 @@ def blastp(ref=None, qry=None, threads=5):
     cmd = blastp_sh.format(ref, qry, qry)
     job = bsub(cmd, cpus=threads)
     waitjob(job)
+    return 0
+
+
+# 0 ref
+# 1 qry
+# 2 output
+blastn_sh = r"""
+makeblastdb -in {0} -dbtype nucl
+blastn  -num_threads 5 -db {0} -query {1} -out {2}.bln -evalue 1e-5 -outfmt 7
+"""
+
+
+def blastn(ref=None, qry=None, threads=5):
+    r"""
+    :param qry:
+    :param ref:
+    :return:
+    """
+    cmd = blastn_sh.format(ref, qry, qry)
+    job = bsub(cmd, name='blast', cpus=5)
+    #waitjob(job)
     return 0
 
 
@@ -48,17 +70,17 @@ def filter_reciprocal_best(bln=None):
             except ValueError:
                 logger.error(line)
                 continue
-            if(bitscore > highest[qry]):
+            if (bitscore > highest[qry]):
                 highest[qry] = bitscore
                 qry_best[qry] = ref
                 qry_line[qry] = line.rstrip()
-            if(bitscore > highest[ref]):
+            if (bitscore > highest[ref]):
                 highest[ref] = bitscore
                 ref_best[ref] = qry
     for k in qry_best:
         this_best_ref = qry_best[k]
-        if(ref_best[this_best_ref] == k):
-            #Reciprocal best
+        if (ref_best[this_best_ref] == k):
+            # Reciprocal best
             print(qry_line[k])
 
 
