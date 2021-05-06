390682763b3c3e490947f5110b22e4c40be5ffee
lhui2010@gmail.com <lhui2010@gmail.com>
2021-05-06 21:19:44 +0800

    New mtei

--- wrapper/iga/project/a188.py
+++ wrapper/iga/project/a188.py
@@ -1462,16 +1462,16 @@ def mtei_union(TIP_table=None):
     with open(TIP_table) as fh:
         for line in fh:
             mylist = line.split()
-            len_left[mylist[6]] = int(mylist[2] - mylist[1])
+            len_left[mylist[6]] = int(mylist[2]) - int(mylist[1]) + 1
             if mylist[6] not in te_left:
-                te_left[mylist[6]] = set(range(int(mylist[12]), int(mylist[13])))
+                te_left[mylist[6]] = set(range(int(mylist[12]), int(mylist[13] + 1)))
             else:
-                te_left.union(set(range(int(mylist[12]), int(mylist[13]))))
+                te_left.union(set(range(int(mylist[12]), int(mylist[13] + 1))))
     for k in len_left:
         a = len_left[k]
         b = len(te_left[k])
         c = b/a
-        print("\t".join([a,b,c]))
+        print("{}\t{}\t{}".format([a,b,c]))
 
 
 if __name__ == "__main__":
