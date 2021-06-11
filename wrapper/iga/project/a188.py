1c0e2bddd312364dcecb0e4a218824093a713530
lhui2010@gmail.com <lhui2010@gmail.com>
2021-06-11 09:13:31 +0800

    qsub

--- wrapper/iga/project/a188.py
+++ wrapper/iga/project/a188.py
@@ -484,7 +484,7 @@ def syri(ref=None, qry=None, threads=6, submit='T', bychr='F', node='rock0[12]')
             qsub(cmd, cpus=threads, name='syri.' + prefix, node=node)
 
 
-def syri_batch(genome_list=None, bychr='F', node='rock0[12]'):
+def syri_batch(genome_list=None, bychr='F', node='rock0[12]', threads=8):
     """
     batch syri wrapper
     :param genome_list: multiple genome fasta, like a.genome b.genome c.genome
@@ -504,7 +504,7 @@ def syri_batch(genome_list=None, bychr='F', node='rock0[12]'):
             if qry == ref:
                 continue
             else:
-                syri(qry, ref, bychr=bychr, node=node)
+                syri(qry, ref, bychr=bychr, node=node, threads=threads)
 
 
 def synal_to_mosaic(synal_file=None, syriout='F', syn_tag='SYNAL', output=''):
