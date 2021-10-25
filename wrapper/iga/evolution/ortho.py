5f2093f9625823a9b784cbf1d550294091c93890
lhui2010@gmail.com <lhui2010@gmail.com>
2021-10-25 21:30:49 +0800

    Update MCScanX pipeline

--- wrapper/iga/evolution/ortho.py
+++ wrapper/iga/evolution/ortho.py
@@ -15,8 +15,8 @@ tail -n +12 {0}.collinearity |sed "s/^#.*/###/; s/.*:\s\+//" > {0}.anchors
 grep -v "#" {0}.anchors |awk '{{print $1"\t"$2}}' > {0}.ortho
 QRY=ae
 REF=ce
-python -m jcvi.graphics.dotplot ${{QRY}}.${{REF}}.anchors
-python -m jcvi.compara.synteny depth --histogram ${{QRY}}.${{REF}}.anchors
+bsub "python -m jcvi.graphics.dotplot ${{QRY}}.${{REF}}.anchors"
+bsub "python -m jcvi.compara.synteny depth --histogram ${{QRY}}.${{REF}}.anchors"
 """
 
 
