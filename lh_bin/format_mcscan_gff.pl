c9a3096040bda7644133898be390c2a24654a802
lhui2010 <lhui2010@gmail.com>
2021-10-25 21:13:19 +0800

    CMD update

--- lh_bin/format_mcscan_gff.pl
+++ lh_bin/format_mcscan_gff.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/env perl
 
 
 #cu00000181      .       mRNA    264475  272937  .       +       .       ID=evm.model.tig00000181.16.2;Parent=evm.TU.tig00000181.16;Name=EVM%20prediction%20tig00000181.16
@@ -7,7 +8,7 @@ while(<>)
     next unless (/mRNA/);
     chomp;
     my @e=split;
-    if(/ID=(.*?);/)
+    if(/ID=(.*?);/ or /ID=(.*)$/ )
     {
         $mRNAID = $1;
         $start{$mRNAID} = $e[3];
