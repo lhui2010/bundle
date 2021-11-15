ead2cab29b710c39fb66ce3e94b28314eddd794a
lhui2010@gmail.com <lhui2010@gmail.com>
2021-11-15 16:14:02 +0800

    Add col2anc.pl

--- wrapper/iga/evolution/ortho.py
+++ wrapper/iga/evolution/ortho.py
@@ -32,7 +32,7 @@ bsub  -R "span[hosts=1]" -q Q104C512G_X4  -o output.%J -e error.%J "python -m jc
 """
 
 
-def mcscanx(prefix1=None, prefix2=None, threads=1, min_gene_in_block=5, max_gene_gap=25, no_html="T", runKs='T',
+def mcscanx(prefix1=None, prefix2=None, threads=4, min_gene_in_block=5, max_gene_gap=25, no_html="T", runKs='T',
             use_grid='T', top_num=10):
     """
     ⭐️️The mcscanx wrapper, execution eg:
