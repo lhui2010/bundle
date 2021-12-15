0604c5e7e02c23172f8579b1b29e93992bc20529
lhui2010 <lhui2010@gmail.com>
2021-12-15 18:59:50 +0800

    fix index error in dmain

--- wrapper/iga/annotation/gff.py
+++ wrapper/iga/annotation/gff.py
@@ -15,7 +15,7 @@ import logging
 import coloredlogs
 
 logger = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG', logger=logger)
+coloredlogs.install(level='INFO', logger=logger)
 
 
 class Feat:
