fe2e7ecc2c4fa2a5d3d84056c9795ece25b47d8c
lhui2010 <lhui2010@gmail.com>
2022-01-16 14:23:18 +0800

    CMD update

--- Rscripts/ks_plot.R
+++ Rscripts/ks_plot.R
@@ -10,9 +10,10 @@ self_kaks_filter = self_kaks[self_kaks$Ks<6,]
 
 # a=data.frame(Ks=self_kaks_filter$Ks, Type=self_kaks_filter$file)
 
-p <- ggplot(self_kaks_filter, aes(Ks, after_stat(density), color = file, linetype="a")) +
-    geom_density(bw = 0.05,alpha=0.5, position="identity")  +
-    facet_wrap(vars(Fam))
+p <- ggplot(self_kaks_filter, aes(Ks, after_stat(density),  linetype="a")) +
+    geom_density(bw = 0.05,alpha=0.5, position="identity")  #+
+#p <- ggplot(self_kaks_filter, aes(Ks, after_stat(density), color = file, linetype="a")) +
+#     facet_wrap(vars(Fam))
 #    facet_grid(rows=vars(Fam))
 ggsave(pdf_name, p, width=16, height=9)
 
