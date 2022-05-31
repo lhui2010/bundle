1a6e76b305f3d4926c4b62fa0b08f0ae25400c56
lhui2010 <lhui2010@gmail.com>
2022-05-31 14:22:49 +0800

    fix on commonWGD on left right problem

--- wrapper/iga/evolution/ortho.py
+++ wrapper/iga/evolution/ortho.py
@@ -318,7 +318,13 @@ def select_block_by_ks(anchor_ks=None, min_ks=0, max_ks=0.7):
 commonWGD_sh = r"""
 export OMP_NUM_THREADS="8";
 selectItem.pl 0 0,1 ../{0}       ../{1} |sed "s/\t/-/" > {0}.left
+# avoid left-right problem
+selectItem.pl 1 0,1 ../{0}       ../{1} |sed "s/\t/-/" >> {0}.left
+
 selectItem.pl 1 0,1 ../{0}       ../{2} |sed "s/\t/-/" > {0}.right
+selectItem.pl 0 0,1 ../{0}       ../{2} |sed "s/\t/-/" > {0}.right
+
+
 selectItem.pl -h    {0}.left  ../{1}.kaks >{0}.left.kaks
 selectItem.pl -h    {0}.right ../{2}.kaks >{0}.right.kaks
 get_ks_peak.py      {0}.left.kaks > ../{0}.left.peak
