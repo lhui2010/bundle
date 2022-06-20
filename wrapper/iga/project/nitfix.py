eac403c9ddfec2a05b0c3872863bf7dd6ad415d9
lhui2010 <lhui2010@gmail.com>
2022-06-20 16:21:40 +0800

    orthogroup2ortholog

--- wrapper/iga/project/nitfix.py
+++ wrapper/iga/project/nitfix.py
@@ -212,6 +212,7 @@ def group2orthologs(orthogroup=None, max_group_size=18, outdir='ortholog_split',
             iter_result = itertools.product(qry_dict[orthogroup], ref_dict[orthogroup])
             for ortho_pair in iter_result:
                 result_db[pair_name] += ("\t".join(ortho_pair)+"\n")
+                logging.info(pair_name)
         with open(op.join(outdir, pair_name + ".ortho"), 'w') as fh:
             fh.write(result_db[pair_name])
     # mkdir(outdir)
