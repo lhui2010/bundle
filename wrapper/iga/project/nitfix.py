feb957db1f859e9fda69a03d87ea137761a3ee55
lhui2010 <lhui2010@gmail.com>
2022-06-29 09:21:07 +0800

    update

--- wrapper/iga/project/nitfix.py
+++ wrapper/iga/project/nitfix.py
@@ -199,7 +199,7 @@ def group2orthologs(orthogroup=None, max_group_size=18, outdir='ortholog_split',
         if (flag):
             species_pairs.append(pair)
     species_pairs = itertools.product(interest_list2, interest_list)
-    logging.info(species_pairs)
+    # logging.info(species_pairs)
     logging.info("Prepare of species pair complete, now writing to files...")
 
     for col in range(start_col, columns_len):
